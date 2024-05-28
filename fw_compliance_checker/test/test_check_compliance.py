9bb222af23570961b72418c561a45a6664b49194
CMoreno04 <cesar.jos.em26@icloud.com>
2024-05-28 17:59:39 -0400

    get_rule function test pass

--- fw_compliance_checker/test/test_check_compliance.py
+++ fw_compliance_checker/test/test_check_compliance.py
@@ -2,12 +2,19 @@
 import pytest
 from src.check_compliance import get_rules, verify_compliance
 
-
+#Test get rules
 def test_get_rules():
-    assert get_rules("url") == "get rules"
+    rules = get_rules()
+    
+    #assert that object returned is an instance of list
+    assert isinstance(rules, list)
+    
+    #assert that the list is not empty
+    assert len(rules) > 0 
 
 
 def test_verify_compliance():
+   
     assert verify_compliance("rule") == "verify compliance"
     
     
