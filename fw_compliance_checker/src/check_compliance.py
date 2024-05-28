e5e686a94b2055690be20d6ec765f34e3be50ecc
CMoreno04 <cesar.jos.em26@icloud.com>
2024-05-28 16:14:17 -0400

    add template functions to main script

---
+++ fw_compliance_checker/src/check_compliance.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+
+def get_rules(url):
+    print("fetches rules")
+    return "get rules"
+  
+    
+def verify_compliance(rule):
+    print("verifies compliance")
+    return "verify compliance"
+
+
+def main():
+    print("initialize process")
+    get_rules("")
+    verify_compliance("")
+    return "main"
+    
+    
+if __name__ == "__main__":
+    main()
