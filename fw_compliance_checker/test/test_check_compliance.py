2e3c76bad0c111db74ef9f4b5378328f0096cda2
CMoreno04 <cesar.jos.em26@icloud.com>
2024-05-28 18:53:59 -0400

    adds verify_compliance test

--- fw_compliance_checker/test/test_check_compliance.py
+++ fw_compliance_checker/test/test_check_compliance.py
@@ -15,7 +15,50 @@ def test_get_rules():
 
 def test_verify_compliance():
    
-    assert 1 == 0
+    compliant_rule = {
+        "RuleId": "1",
+        "FromPort": 100,
+        "ToPort": 200,
+        "IpRanges": ["192.168.1.1/32"],
+        "Action": "Allow",
+        "Direction": "Ingress"
+    }
+    
+    non_compliant_rule = {
+        "RuleId": "2",
+        "FromPort": 22,
+        "ToPort": 22,
+        "IpRanges": ["236.216.246.119/32"],
+        "Action": "Allow",
+        "Direction": "Ingress"
+    }
+    
+    compliant_rule_non_ingress = {
+        "RuleId": "3",
+        "FromPort": 22,
+        "ToPort": 22,
+        "IpRanges": ["236.216.246.119/32"],
+        "Action": "Allow",
+        "Direction": "Egress"
+    }
+    
+    compliant_rule_deny = {
+        "RuleId": "4",
+        "FromPort": 80,
+        "ToPort": 80,
+        "IpRanges": ["236.216.246.119/32"],
+        "Action": "Deny",
+        "Direction": "Ingress"
+    }
+
+    #assert if rule is compliant by port number
+    assert verify_compliance(compliant_rule) == "COMPLIANT"
+    
+    #assert if rule is non compliant by port number
+    assert verify_compliance(non_compliant_rule) == "NON_COMPLIANT"
+    
+    #assert if rule is compliant by Direction
+    assert verify_compliance(compliant_rule_non_ingress) == "COMPLIANT"
     
     
 if __name__ == "__main__":
